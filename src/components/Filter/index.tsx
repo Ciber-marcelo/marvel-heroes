08524cfbb2a090fcc992d2066c4630b26af390cd
Ciber-marcelo <marcelones125@gmail.com>
2024-08-15 13:09:51 -0300

    feat: create Filter

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+
+type Props = {
+  image: any
+  text: string
+  onClick?: any
+}
+
+export default function Filter({ image, text, onClick }: Props) {
+  
+  return (
+    <button 
+      className='flex items-center gap-4 p-2 rounded-md hover:bg-color2 active:bg-color3' 
+      onClick={onClick}
+    >
+      <Image
+        className='w-auto h-auto' 
+        width={20}
+        height={20}
+        src={image}
+        alt="image hero"
+        priority={true}
+      />
+      <div className="text-color1">{text}</div>
+    </button>
+  );
+}
